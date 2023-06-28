213a3a9691ca37e74cdd2207c8b380b117228e40
alndra7 <al.go9871@gmail.com>
2023-06-28 22:02:23 +0300

    added select on cards

--- week23_app/src/components/Card.jsx
+++ week23_app/src/components/Card.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.scss';
 
 function Card(props) {
+    const [pressed, setPressed] = useState();
+
+    const handleChange = () => {
+        setPressed(pressed ? '' : 'picked');
+        console.log('Получен фокус на поле ввода');
+    }
+
     return (
-        <div className={props.name}>
+        <div className={`${props.name} ${pressed}`}
+            onPointerEnter={handleChange} onPointerLeave={handleChange}>
             <div className='header'>Безлимитный {props.number}</div>
             <div className='price'>{props.number} руб/месяц</div>
             <div className='speed'>до {props.speed} Мбит/сек</div>
             <div className='info'>Объем включенного трафика не ограничен</div>
         </div>
-
     );
 }
 
